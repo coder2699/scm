c3b26bc524476c4fbdb750e73ee8da0d746d3eb0
coder2699 <pranshu0414@gmail.com>
2024-08-31 13:40:05 +0530

    backend setup ready for apis

--- src/main/java/com/myproject/scm/Controllers/PageController.java
+++ src/main/java/com/myproject/scm/Controllers/PageController.java
@@ -3,6 +3,9 @@ package com.myproject.scm.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController {
@@ -16,7 +19,6 @@ public class PageController {
 
 
     // about route
-
     @RequestMapping("/about")
     public String aboutPage(Model model) {
         model.addAttribute("isLogin", true);
@@ -25,10 +27,28 @@ public class PageController {
     }
 
     // services
-
     @RequestMapping("/services")
     public String servicesPage() {
         System.out.println("services page loading");
         return "services";
     }
+
+    // contact
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    // login
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // contact
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+    
 }
